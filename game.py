4cdba0c3bb89d41d0e2ff85a9723f4a657854ad2
KernelPanic <me@jakeirvine.com>
2019-10-03 15:55:42 +0100

    first exmaple

---
+++ game.py
@@ -0,0 +1,23 @@
+import random
+
+# This game generates a random number, and then
+sees if you can guess the number
+
+random = 5
+x = random.randint(1, 100)
+print('Welcome to my game! Can you guess my number? If you can't, try again')
+while true:
+
+    number = imput(''Please input your guess'', end=4)
+
+    if [x = numbre] {
+    print "You win!"
+                    return
+    } else if [x > number]:
+        print('too big!')
+    else if x < random:
+        print(too small!)
+
+
+print('The random number was' + str(random))
+
